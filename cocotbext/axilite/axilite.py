307030f34593651f8db89134497d2624e1655dd5
Fatih Yazıcı <fatihyazici67@gmail.com>
2021-11-06 23:14:10 +0100

    cocotb '<='  assignment deprecation fix

--- cocotbext/axilite/axilite.py
+++ cocotbext/axilite/axilite.py
@@ -31,29 +31,29 @@ class AXI4LiteMaster(BusDriver):
         self.bus.rready.setimmediatevalue(0)
 
     async def write(self, waddr, wdata, timeout=None):
-        self.bus.awvalid <= 1
-        self.bus.awaddr <= waddr
-        self.bus.wvalid <= 1
-        self.bus.wdata <= wdata
+        self.bus.awvalid.value = 1
+        self.bus.awaddr.value = waddr
+        self.bus.wvalid.value = 1
+        self.bus.wdata.value = wdata
         await RisingEdge(self.clock)
 
         while True:
             if self.bus.awready.value:
-                self.bus.awvalid <= 0
+                self.bus.awvalid.value = 0
             if self.bus.wready.value:
-                self.bus.wvalid <= 0
+                self.bus.wvalid.value = 0
             if (self.bus.awvalid == 0 and self.bus.wvalid == 0) or timeout == 0:
                 break
             if timeout is not None:
                 timeout -= 1
             await RisingEdge(self.clock)
         
-        self.bus.bready <= 1
+        self.bus.bready.value = 1
         await RisingEdge(self.clock)
 
         while True:
             if self.bus.bvalid.value or timeout == 0:
-                self.bus.bready <= 0
+                self.bus.bready.value = 0
                 break
             if timeout is not None:
                 timeout -= 1
@@ -65,24 +65,24 @@ class AXI4LiteMaster(BusDriver):
         return self.bus.bresp.value
     
     async def read(self, raddr, timeout=None):
-        self.bus.arvalid <= 1
-        self.bus.araddr <= raddr
+        self.bus.arvalid.value = 1
+        self.bus.araddr.value = raddr
         await RisingEdge(self.clock)
 
         while True:
             if self.bus.arready.value or timeout == 0:
-                self.bus.arvalid <= 0
+                self.bus.arvalid.value = 0
                 break
             if timeout is not None:
                 timeout -= 1
             await RisingEdge(self.clock)
         
-        self.bus.rready <= 1
+        self.bus.rready.value = 1
         await RisingEdge(self.clock)
 
         while True:
             if self.bus.rvalid.value or timeout == 0:
-                self.bus.rready <= 0
+                self.bus.rready.value = 0
                 break
             if timeout is not None:
                 timeout -= 1
@@ -123,36 +123,36 @@ class AXI4LiteSlaveMem(BusDriver):
             # "wait for valid" type slave
             while True:
                 if self.bus.awvalid.value and self.bus.wvalid.value:
-                    self.bus.awready <= 1
-                    self.bus.wready <= 1
+                    self.bus.awready.value = 1
+                    self.bus.wready.value = 1
                     break
                 if self.bus.arvalid.value:
-                    self.bus.arready <= 1
+                    self.bus.arready.value = 1
                     break
                 await RisingEdge(self.clock)
             await RisingEdge(self.clock)
 
-            self.bus.awready <= 0
-            self.bus.wready <= 0
-            self.bus.arready <= 0
+            self.bus.awready.value = 0
+            self.bus.wready.value = 0
+            self.bus.arready.value = 0
             if self.bus.awvalid.value:
                 self.mem[str(self.bus.awaddr.value)] = self.bus.wdata.value
-                self.bus.bvalid <= 1
-                self.bus.bresp <= 0
+                self.bus.bvalid.value = 1
+                self.bus.bresp.value = 0
             else:
-                self.bus.rvalid <= 1
+                self.bus.rvalid.value = 1
                 if str(self.bus.araddr.value) in self.mem:
-                    self.bus.rdata <= self.mem[str(self.bus.araddr.value)]
-                    self.bus.rresp <= 0
+                    self.bus.rdata.value = self.mem[str(self.bus.araddr.value)]
+                    self.bus.rresp.value = 0
                 else:
-                    self.bus.rresp <= 1
+                    self.bus.rresp.value = 1
             await RisingEdge(self.clock)
 
             while True:
                 if self.bus.bvalid.value and self.bus.bready.value:
-                    self.bus.bvalid <= 0
+                    self.bus.bvalid.value = 0
                     break
                 if self.bus.rvalid.value and self.bus.rready.value:
-                    self.bus.rvalid <= 0
+                    self.bus.rvalid.value = 0
                     break
                 await RisingEdge(self.clock)
